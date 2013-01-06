5c9d119980418c7b6a2d36d01cf097c14b5fa606
Daniel Aquino <mr.danielaquino@gmail.com>
2013-01-06 09:11:08 -0500

    updating make.sh to remap dylibs in the projectx binary on osx to use @executable_path

--- make.sh
+++ make.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -x
 cd "$(dirname -- "$0")"
-export PKG_CONFIG_PATH="$PWD/lib/pkgconfig"
+DIR=${PWD/*\/} # path to this folder
+LIB=$PWD/lib # path to lib folder
+export PKG_CONFIG_PATH="$LIB/pkgconfig"
 cd .. && make "$@"
+
+# on osx we need to remap the path to dynamic libs
+uname -a | grep -q Darwin &&
+dyldinfo -dylibs ./projectx |
+grep dylib |
+while read -r path; do
+	file=${path/*\/}
+	[[ -f "$LIB/$file" ]] || continue
+	install_name_tool -change \
+		"$path" \
+		"@executable_path/$DIR/lib/$file" \
+		./projectx ||
+			exit 1
+done
+exit 0
