504d7af403540d55e21361248e96fcc2c2d4de14
Daniel Aquino <mr.danielaquino@gmail.com>
2013-01-20 23:17:02 -0500

    catching errors from make and exiting script

--- make.sh
+++ make.sh
@@ -3,7 +3,8 @@ cd "$(dirname -- "$0")"
 DIR=${PWD/*\/} # path to this folder
 LIB=$PWD/lib # path to lib folder
 export PKG_CONFIG_PATH="$LIB/pkgconfig"
-cd .. && make "$@"
+cd ..
+make "$@" || exit 1
 
 # on osx we need to remap the path to dynamic libs
 uname -a | grep -q Darwin &&
