12419578c0f1d554e22d67c8c1b7133c040698f1
Daniel Aquino <mr.danielaquino@gmail.com>
2013-08-08 19:49:39 -0400

    Explicitly define order that build scripts run
    Previously the for loop could/would build things out of order causing
    builds to fail.

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,19 @@
 cd "$(dirname -- "$0")"
 mkdir -p ../lib/pkgconfig 2>/dev/null
 rm build.log
-for b in *.build; do
-	./$b >> build.log || exit 1
-done
+set -e # die if any of the following fail
+{
+
+	# these must generally be built in this order
+	./zlib-1.2.5.build
+	./libpng-1.4.4.build
+	./lua-5.1.4.build
+	./luasocket-2.0.2.build
+
+	# these are independent
+	./enet-1.2.5.build
+	./SDL-1.2.14.build
+	./openal-soft-1.14.build
+
+} 2>&1 | tee build.log
 echo "Done!"
