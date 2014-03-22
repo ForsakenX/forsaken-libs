8cb08dbc7b3d328a0e3122611518e920305ad7da
Daniel Aquino <mr.danielaquino@gmail.com>
2014-03-22 00:16:00 -0400

    add flag to rebuild libs only when requested.
    Previously we would always rebuild everything every time that build.sh
    was called which could be a bit annoying if you have a failing build.
    Now it will track which ones have successfully built and only rum them
    against if requested.

--- src/build.sh
+++ src/build.sh
@@ -3,18 +3,29 @@ cd "$(dirname -- "$0")"
 mkdir -p ../lib/pkgconfig 2>/dev/null
 rm build.log
 set -e # die if any of the following fail
+# pass --rebuild if you want to rebuild everything
+rebuild=false; [[ $* =~ --rebuild ]] && rebuild=true
+function run
+{
+	if ! $rebuild && [[ -f ${1}.built ]]; then
+		echo "rm ${1}.built if you want to rebuild."
+		return 0
+	fi
+	"$@" || return 1
+	touch ${1}.built
+}
 {
 
 	# these must generally be built in this order
-	./zlib-1.2.5.build
-	./libpng-1.4.4.build
-	./lua-5.1.4.build
-	./luasocket-2.0.2.build
+	run ./zlib-1.2.5.build
+	run ./libpng-1.4.4.build
+	run ./lua-5.1.4.build
+	run ./luasocket-2.0.2.build
 
 	# these are independent
-	./enet-1.2.5.build
-	./SDL-1.2.build.latest
-	./openal-soft-1.14.build
+	run ./enet-1.2.5.build
+	run ./SDL-1.2.build.latest
+	run ./openal-soft-1.14.build
 
 } 2>&1 | tee build.log
 if [[ ${PIPESTATUS[0]} != 0 ]]; then
